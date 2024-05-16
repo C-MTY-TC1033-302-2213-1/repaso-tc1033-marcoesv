1c2efcf5e645136068824ae0652ce72e7a940183
marcoesv <A01286072@tec.mx>
2024-05-15 23:26:04 -0600

    absolute value in area

--- Triangulo.cpp
+++ Triangulo.cpp
@@ -39,7 +39,7 @@ double Triangulo::perimetro(){
 return v2.calculaDistancia(v1) + v3.calculaDistancia(v2) + v1.calculaDistancia(v3);
 }
 double Triangulo::area(){
-return 0.5 * ((v1.getX() * v2.getY()) + (v2.getX() * v3.getY()) + (v3.getX() * v1.getY())- ((v1.getX() * v3.getY()) + (v3.getX() * v2.getY())+ (v2.getX() * v1.getY())) );
+return 0.5 * abs(((v1.getX() * v2.getY()) + (v2.getX() * v3.getY()) + (v3.getX() * v1.getY())- ((v1.getX() * v3.getY()) + (v3.getX() * v2.getY())+ (v2.getX() * v1.getY())) ));
 }
 string Triangulo::str(){
 return v1.str() + ','+ v2.str() + ',' + v3.str();
